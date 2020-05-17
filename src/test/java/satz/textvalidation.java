16c9c7cf01ce94aead5a06451e1bdde57ab3c47c
satheesh <satheesh_vg@yahoo.in>
2020-05-17 15:30:31 +0530

    m

--- src/test/java/satz/textvalidation.java
+++ src/test/java/satz/textvalidation.java
@@ -27,6 +27,7 @@ public class textvalidation extends base {
 		System.out.println("two");
 		System.out.println("three");
 		System.out.println("four");
+		System.out.println("five");
 				
 		}
 	@AfterTest
